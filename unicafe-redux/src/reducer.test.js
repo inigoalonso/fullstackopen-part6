661c107c44cbd1998f4cbcc94ea71096f510be74
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-12 15:37:12 +0200

    6.1 & 6.2

--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -32,4 +32,102 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero resets all values', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 5,
+      ok: 3,
+      bad: 2
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('unknown action does not change anything', () => {
+    const action = {
+      type: 'UNKNOWN'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('multiple actions are handled', () => {
+    const actions = [
+      { type: 'GOOD' },
+      { type: 'GOOD' },
+      { type: 'BAD' },
+      { type: 'OK' }
+    ]
+    const state = initialState
+
+    deepFreeze(state)
+    const finalState = actions.reduce(counterReducer, state)
+    expect(finalState).toEqual({
+      good: 2,
+      ok: 1,
+      bad: 1
+    })
+  })
+
+  test('multiple actions are handled, part 2', () => {
+    const actions = [
+      { type: 'GOOD' },
+      { type: 'GOOD' },
+      { type: 'BAD' },
+      { type: 'OK' },
+      { type: 'ZERO' },
+      { type: 'BAD' }
+    ]
+    const state = initialState
+
+    deepFreeze(state)
+    const finalState = actions.reduce(counterReducer, state)
+    expect(finalState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
 })
