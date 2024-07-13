91642805030c6659261a5ebd3692ca1247dcf93e
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-13 15:52:47 +0200

    6.10

--- anecdotes-redux/src/App.jsx
+++ anecdotes-redux/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import anecdoteService from './services/anecdotes'
+import { createAnecdote } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(createAnecdote(anecdotes)))
+  }, [dispatch])
 
   return (
     <div>
--- anecdotes-redux/src/reducers/filterReducer.js
+++ anecdotes-redux/src/reducers/filterReducer.js
@@ -1,17 +1,32 @@
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+import { createSlice, current } from '@reduxjs/toolkit'
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter
+//   }
+// }
+
+// const filterReducer = (state = '', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export default filterReducer
