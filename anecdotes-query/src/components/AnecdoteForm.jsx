39abe01080a49af171dfe2617ad7cb86d040cfb0
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-14 11:23:09 +0200

    6.21

--- anecdotes-query/src/components/AnecdoteForm.jsx
+++ anecdotes-query/src/components/AnecdoteForm.jsx
@@ -1,10 +1,22 @@
+import { useMutation, useQueryClient} from '@tanstack/react-query'
+import { postAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  
+  const newAnecdoteMutation = useMutation({
+    mutationFn: postAnecdote, 
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    },
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newAnecdoteMutation.mutate(content)
 }
 
   return (
--- anecdotes-query/src/requests.js
+++ anecdotes-query/src/requests.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 
 export const getAnecdotes = () =>
   axios.get('http://localhost:3001/anecdotes').then(res => res.data)
+
+export const postAnecdote = (content) =>
+  axios.post('http://localhost:3001/anecdotes', { content, votes: 0 }).then(res => res.data)
