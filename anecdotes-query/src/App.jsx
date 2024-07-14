88b8d3666144ece8871171f034b38044b9f562f0
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-14 10:55:13 +0200

    6.20

--- anecdotes-query/src/App.jsx
+++ anecdotes-query/src/App.jsx
@@ -1,3 +1,6 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
@@ -7,13 +10,25 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    onError: (error) => {
+      console.error("Error fetching anecdotes:", error);
+    }
+  })
+
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems with the server</div>
+  }
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
